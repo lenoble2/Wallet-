14c822485bf32187e71170a78fda8e4a592f01f7
TonNouveauPseudo <ton-nouvel-email@exemple.com>
2026-05-17 15:16:32 +0000

    Feat: Ajout des fichiers PWA requis pour l'installation globale

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'leanpay-v1';
+const assets = ['/login.html', '/icon.png', '/echange.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
+
